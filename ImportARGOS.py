9535426b6ce5aeea265dda668a2e91900ff11d54
juliageschke <42789687+juliageschke@users.noreply.github.com>
2018-10-15 12:35:40 -0400

    Reads and loops through each record in a single ARGOS file

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -16,3 +16,32 @@ import sys, os, arcpy
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+#Construct a while loop to iterate through all lines in the datafile
+
+#Open the ARGOS data file for reading
+inputFileObj = open(inputFile, 'r')
+
+#Get the first line of data, so we can use a while loop
+lineString = inputFileObj.readline()
+while lineString:
+    #Set code to run only if the line contains the string "Date: "
+    if("Date :" in lineString):
+        #parse the line into a list
+        lineData=lineString.split()
+        #extract attributes from the datum header line
+        tagID=lineData[0]
+        #extract location info from the next line
+        line2String=inputFileObj.readline()
+        #parse the line into a list
+        line2Data=line2String.split()
+        #extract the data we need to variables
+        obsLat=line2Data[2]
+        obsLon=line2Data[5]
+        #print results to see how we're doing
+        print(tagID,"Lat:"+obsLat,"Long:"+obsLon)
+    #Move to the next line so the while loop progresses
+    lineString=inputFileObj.readline()
+
+#close the file object
+inputFileObj.close()
